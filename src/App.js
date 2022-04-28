e711f34967a5363dd4dc65bb4de4eda9dbbe7d25
Vikrant <vikrant@tothenew.com>
2022-04-28 14:36:26 +0530

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
+import { useState } from 'react';
+// import About from './components/About';
 import Form from './components/Form';
 import Navbar from './components/Navbar';
 
 function App() {
+  const [Mode, setMode] = useState('light')
+  const toggleDarkMode=()=>{
+    if(Mode==='dark'){
+      setMode('light')
+      document.body.style.backgroundColor='white';
+    }
+    else{
+      setMode('dark')
+      document.body.style.backgroundColor='#042743';
+    }
+  }
+  
   return (
     <>
-    <Navbar title="TextApplication"/>
+    <Navbar title="TextApplication" mode={Mode} toggleDarkMode={toggleDarkMode}/>
+
     <div className="container">
-    <Form title="TextForm"/>
+    <Form title="TextForm" mode={Mode}/>
     </div>
+
+    {/* <About/> */}
+
+    
     
     </>
      );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,73 +1,72 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 function Form(props) {
+  const [text, settext] = useState("");
 
-  const [text, settext] = useState("")
+  const handleClick = () => {
+    const upperCaseText = text.toUpperCase();
+    settext(upperCaseText);
+  };
 
-  const handleClick=()=>{
-    const upperCaseText=text.toUpperCase();
-    settext(upperCaseText)
-  }
+  const handleLowClick = () => {
+    const lowerCaseText = text.toLowerCase();
+    settext(lowerCaseText);
+  };
 
-  const handleLowClick=()=>{
-    const lowerCaseText=text.toLowerCase();
-    settext(lowerCaseText)
-  }
+  const handleCapitalise = () => {
+    const words = text.split(" ");
 
-  const handleCapitalise=()=>{
-    const words=text.split(' ');
+    let capitalisedString = [];
 
-    let capitalisedString=[];
-
-    words.forEach((word)=>{
-      word=word[0].toUpperCase()+word.substring(1);
+    words.forEach((word) => {
+      word = word[0].toUpperCase() + word.substring(1);
       capitalisedString.push(word);
-    })
-    capitalisedString=capitalisedString.join(" ");
-    settext(capitalisedString)
-    
-  }
+    });
+    capitalisedString = capitalisedString.join(" ");
+    settext(capitalisedString);
+  };
 
-  const handleOnChange=(event)=>{
-    settext(event.target.value)
-  }
+  const handleOnChange = (event) => {
+    settext(event.target.value);
+  };
   return (
     <>
-        <h1>{props.title}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="formBox"
-          rows="8"
-          value={text}
-          onChange={handleOnChange}
-        />
+      <div className="container" style={{
+              color:props.mode==='dark'?'white':'black',
+            }}>
+        <h1 style={{}}>{props.title}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="formBox"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor:props.mode==='dark'?'#042743':'white',
+              color:props.mode==='dark'?'white':'black'
+            }}
+          />
+        </div>
+
+        <button className="btn btn-primary mx-2" onClick={handleClick}>
+          Convert to Upper Case
+        </button>
+
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+          Convert to Lower Case
+        </button>
+
+        <button className="btn btn-primary" onClick={handleCapitalise}>
+          Capitalise the First Letter
+        </button>
+
+        <h1>Summary</h1>
+
+        <p>Words:{text.split(" ").length}</p>
+        <p>Characters:{text.length}</p>
       </div>
-
-      <button className="btn btn-primary mx-2" onClick={handleClick}>
-        Convert to Upper Case
-      </button>
-
-      <button className="btn btn-primary mx-2" onClick={handleLowClick}>
-        Convert to Lower Case
-      </button>
-
-
-      <button className="btn btn-primary" onClick={handleCapitalise}>
-        Capitalise the First Letter
-      </button>
-
-      <h1>Summary</h1>
-
-      <p>Words:{text.split(" ").length}</p>
-      <p>Characters:{text.length}</p>
-      
-
     </>
-
-    
-
-
   );
 }
 
